2de69cf07a730d2cdb9e4275a2816af29fcfd962
José Daniel Velásquez Orozco <jdvela158@gmail.com>
2025-02-07 14:32:41 -0600

    Archivo de la clase 5 subido correctamente.

---
+++ HotDog.py
@@ -0,0 +1,31 @@
+class HotDog:
+    def __init__(self, ingredents: list):
+        self.ingredents = ingredents
+
+    @staticmethod
+    def verifyIngredents(ingr):
+        if ingr == "Lechuga":
+            print(f"{ingr} No es un ingrediente del hotdog")
+            return False
+        else:
+            return True
+
+    def getIngredents(self):
+        return self.ingredents
+
+ingrs = ["Pan", "Salchicha", "Mayonesa", "Mostaza"]
+ingrs2 = ["Pan", "Salchicha", "Mayonesa", "Mostaza"]
+if all(HotDog.verifyIngredents(i) for i in ingrs):
+    hotdog = HotDog(ingrs)
+    print("HotDog creado correctamente")
+    print(hotdog.getIngredents())
+
+if all(HotDog.verifyIngredents(i) for i in ingrs2):
+    hotdog = HotDog(ingrs)
+    print("HotDog creado correctamente")
+
+'''
+hotdog = HotDog(["Pan", "Salchicha", "Mayonesa", "Mostaza"])
+if all(hotdog.verifyIngredents(i) for i in hotdog.ingredents):
+    print("Hotdog creado")
+'''
